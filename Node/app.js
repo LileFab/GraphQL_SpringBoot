77668e3d5040bb2530182cd66420bbb5eb687f03
Fabien <backspeen@gmail.com>
2024-03-17 22:29:13 +0100

    Update app.js

--- Node/app.js
+++ Node/app.js
@@ -1,22 +1,132 @@
-const { request, gql } = require('graphql-request')
+const fetch = require('node-fetch');
 
-const query = `
-    {
-        getAllPlayers {
-            id
-            name
-            age
-            height
-              team
-        }
+async function getAllPlayers() {
+  const url = 'http://localhost:8080/graphql';
+
+  const query = `
+    query {
+      getAllPlayers {
+        id
+        name
+        age
+        height
+        team
+      }
     }
-`
+  `;
 
-request('http://localhost:8080/graphql', query)
-    .then(
-        (data) => {
-            data.getAllPlayers.forEach(element => {
-                console.log(`Found player #${element.id} with name ${element.name}`);
-            });
-        }
-    )
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query }),
+  };
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log('All players:', data.data.getAllPlayers);
+  }
+
+
+async function getAllTeams() {
+  const url = 'http://localhost:8080/graphql';
+
+  const query = `
+    query {
+      getAllTeams {
+        id
+        name
+        city
+        creationDate
+      }
+    }
+  `;
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query }),
+  };
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log('All teams:', data.data.getAllTeams);
+  }
+
+
+async function addPlayer() {
+  const url = 'http://localhost:8080/graphql';
+
+  const mutation = `
+    mutation {
+      addPlayer(
+        id: "123",
+        name: "John Doe",
+        age: 25,
+        height: 180,
+        team: "Team1"
+      ) {
+        id
+        name
+        age
+        height
+        team
+      }
+    }
+  `;
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query: mutation }),
+  };
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log('Player added:', data.data.addPlayer);
+  }
+
+
+async function addTeam() {
+  const url = 'http://localhost:8080/graphql';
+
+  const mutation = `
+    mutation {
+      addTeam(
+        id: "Team1",
+        name: "Team A",
+        city: "City A",
+        creationDate: "2024-03-17"
+      ) {
+        id
+        name
+        city
+        creationDate
+      }
+    }
+  `;
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query: mutation }),
+  };
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log('Team added:', data.data.addTeam);
+  }
+
+
+getAllPlayers();
+getAllTeams();
+
+addPlayer();
+addTeam();
